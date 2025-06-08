779f5684389bde2ac7f8d3009f63d10a3dcba406
JohnMV <johnmarulanda74@gmail.com>
2025-06-08 18:44:49 -0500

    refactor(routing): update router and component imports for consistency
    - Replace BrowserRouter with HashRouter for better deployment compatibility
    - Rename RadialMenu to MenuDock and GestureSettings to InfoGestures for clarity
    - Update route paths to match renamed components

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 
-import { RadialMenu } from './components/RadialMenu'
-import HelpSupportV2 from './pages/HelpSupportV2'
+import { RadialMenu } from './components/MenuDock'
+import HelpSupportV2 from './pages/HelpSupport'
 import IntroAnimation from './pages/IntroAnimation'
 
 import ControlPanel from './pages/ControlPanel'
-import GestureSettings from './pages/GestureSeettings'
+import InfoGestures from './pages/InfoGestures'
 import GestureTesting from './pages/GestureTesting'
 import LandingPage from './pages/LandingPage'
-import SettingsInterface from './pages/Settings2'
+import SettingsInterface from './pages/Settings'
 
 function App(): JSX.Element {
   return (
-    <BrowserRouter>
+    <Router>
       {/* RadialMenu se mostrará en las páginas especificadas */}
       <RadialMenu />
 
@@ -21,14 +21,14 @@ function App(): JSX.Element {
         <Route path="/home" element={<LandingPage />} />
 
         <Route path="/detection-test" element={<GestureTesting />} />
-        <Route path="/detection-settings" element={<GestureSettings />} />
+        <Route path="/detection-info" element={<InfoGestures />} />
 
         <Route path="/settings" element={<SettingsInterface />} />
 
         <Route path="/help" element={<HelpSupportV2 />} />
         <Route path="/control-panel" element={<ControlPanel />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   )
 }
 
